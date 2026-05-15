e5daef9f1f9e2ce14f2bbfefaa44e62e2fb87afb
edwardocollins3 <edwardocollins3@email.com>
2026-05-15 20:34:00 +0300

    Remove duplicate email configuration settings

--- fleeting_logistics/settings.py
+++ fleeting_logistics/settings.py
@@ -176,17 +176,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
-ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'Revolutionnyz@protonmail.com')
-
-
 # Authentication settings
 LOGIN_URL = '/login/'
 LOGOUT_REDIRECT_URL = '/'
